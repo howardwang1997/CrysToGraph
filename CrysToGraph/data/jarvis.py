7dee7fca42544df8d68e1373cc53d11e05701b0e
howardwang1997 <howardwang1997@gmail.com>
2023-12-18 19:35:52 +0800

    add label_2

--- CrysToGraph/data/jarvis.py
+++ CrysToGraph/data/jarvis.py
@@ -38,9 +38,13 @@ def split_dataset(dataset, ratio=0.9, seed=42):
     return train, val
 
 
-def process_jarvis_dataset(dataset, work, label, train_idx, val_idx):
+def process_jarvis_dataset(dataset, work, label, train_idx, val_idx, label_2=''):
+    second_label = False
     data_t, data_v = [dataset[i] for i in train_idx], [dataset[i] for i in val_idx]
     l_t, l_v = [data[label] for data in data_t], [data[label] for data in data_v]
+    if label_2 != '':
+        second_label = True
+        l2_t, l2_v = [data[label_2] for data in data_t], [data[label_2] for data in data_v]
     for i in tqdm(range(len(data_t))):
         a = Atoms.from_dict(data_t[i]['atoms'])
         a.write_cif(f'{work}/train/raw/{i}.cif')
@@ -50,6 +54,9 @@ def process_jarvis_dataset(dataset, work, label, train_idx, val_idx):
     joblib.dump((train_idx, val_idx), f'{work}/tvs.jbl')
     joblib.dump(l_t, f'{work}/train/labels.jbl')
     joblib.dump(l_v, f'{work}/val/labels.jbl')
+    if second_label:
+        joblib.dump(l2_t, f'{work}/train/labels_2.jbl')
+        joblib.dump(l2_v, f'{work}/val/labels_2.jbl')
 
 
 def main():
@@ -57,6 +64,7 @@ def main():
     parser.add_argument('dataset', type=str, help='dataset to be processed, must be jarvis dataset.')
     parser.add_argument('--work', type=str, help='target directory.')
     parser.add_argument('--label', type=str, help='label name')
+    parser.add_argument('--label_2', type=str, help='another label name', default='')
     parser.add_argument('--ratio', type=float, default=0.9, help='ratio of train set')
     parser.add_argument('--split', type=str, help='path to split file')
     parser.add_argument('--random_seed', type=int, help='random seed for dataset split', default=42)
@@ -71,7 +79,7 @@ def main():
     else:
         train_idx, val_idx = split_dataset(dataset, args.ratio, args.random_seed)
 
-    process_jarvis_dataset(dataset, args.work, args.label, train_idx, val_idx)
+    process_jarvis_dataset(dataset, args.work, args.label, train_idx, val_idx, label_2=args.label_2)
 
 
 if __name__ == '__name__':
